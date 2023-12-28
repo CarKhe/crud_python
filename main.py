5fe6562d960b31ff9380e6b454174d22b6d663f2
carkhe <carloskiller177@gmail.com>
2023-12-28 14:25:08 -0600

    Creando la primera ventana

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+class Ventana(Frame):
+    def __init__(self,master=None):
+        super().__init__(master,width=680,height=260)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+    
+    def create_widgets(self):
+        pass
+
+
+
+def main():
+    root = Tk()
+    root.wm_title("Crud Python Mysql")
+    app = Ventana(root)
+    app.mainloop()
+
+
+
+
+
+
+
+
+if __name__ == '__main__': 
+    main()
